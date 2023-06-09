0aaf32fd76c9c4ebc7b6f442dcb0d29c0c3e3c91
Allen Labrague <[allenlabrague06@gmail.com]>
2023-06-09 10:20:28 +0800

    Done side bar

---
+++ data/collection.js
@@ -0,0 +1,27 @@
+const collectionsNft = [
+  {
+    mainImage: "/images/art/big-art.png",
+    secondaryImage: "/images/art/second-art.png",
+    thirdImage: "/images/art/second-art2.png",
+    title: "DSGN Animals",
+    authorAvatar: "/images/mini-profile-avatar/mini-avatar1.png",
+    authorName: "MrFox",
+  },
+  {
+    mainImage: "/images/art/big-art.png",
+    secondaryImage: "/images/art/second-art.png",
+    thirdImage: "/images/art/second-art2.png",
+    title: "Magic Mushrooms",
+    authorAvatar: "/images/mini-profile-avatar/mini-avatar1.png",
+    authorName: "Shroomie",
+  },
+  {
+    mainImage: "/images/art/big-art.png",
+    secondaryImage: "/images/art/second-art.png",
+    thirdImage: "/images/art/second-art2.png",
+    title: "Disco Machines",
+    authorAvatar: "/images/mini-profile-avatar/mini-avatar1.png",
+    authorName: "BeKind2Robots",
+  },
+];
+// This is wrong fix it again tomorrow
