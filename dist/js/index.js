459055e0a740770f28ce1f81dac64a8c1e3b0365
Allen Labrague <[allenlabrague06@gmail.com]>
2023-06-17 12:43:32 +0800

    first commit

--- dist/js/index.js
+++ dist/js/index.js
@@ -6,7 +6,7 @@ hamburgerBtn.addEventListener("click", () => {
   menuToggle.classList.toggle("is-active");
 });
 
-const startingMinutes = 5;
+const startingMinutes = 1;
 let time = startingMinutes * 60;
 
 const countDownEl = document.getElementById("countDown");
@@ -17,7 +17,7 @@ function updateCountDown() {
   const minutes = Math.floor(time / 60);
   let seconds = time % 60;
 
-  seconds = seconds < 10 ? "00" : seconds;
+  seconds = seconds < 1 ? "00" : seconds;
 
   countDownEl.innerHTML = `${minutes} : ${seconds}`;
   time--;
