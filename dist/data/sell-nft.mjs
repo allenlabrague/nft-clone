aee00286aab9219c51d5fdd17145d46556e325db
Allen Labrague <[allenlabrague06@gmail.com]>
2023-06-18 13:58:44 +0800

    first commit

--- dist/data/sell-nft.mjs
+++ dist/data/sell-nft.mjs
@@ -2,7 +2,7 @@ const nfts = [
   {
     image: "../images/pictures/buy-sell-art2.png",
     title: "Distant Galaxy",
-    avatar: "/images/mini-profile-avatar/mini-avatar4.png",
+    avatar: "../images/mini-profile-avatar/mini-avatar4.png",
     avatarName: "MoonDancer",
     price: 1.63,
     bid: 0.33,
@@ -10,7 +10,7 @@ const nfts = [
   {
     image: "../images/pictures/buy-sell-art.png",
     title: "Life On Edena",
-    avatar: "/images/mini-profile-avatar/mini-avatar4.png",
+    avatar: "../images/mini-profile-avatar/mini-avatar4.png",
     avatarName: "NebulaKid",
     price: 2.63,
     bid: 1.33,
@@ -18,7 +18,7 @@ const nfts = [
   {
     image: "../images/pictures/buy-sell-art1.png",
     title: "AstroFiction",
-    avatar: "/images/mini-profile-avatar/mini-avatar4.png",
+    avatar: "../images/mini-profile-avatar/mini-avatar4.png",
     avatarName: "SpaceOne",
     price: 6.63,
     bid: 10.33,
